c6f129aa2cbde0c7c8621f90afa2401babc05fab
Pudong Zheng <tianshi8650@gmail.com>
2022-06-16 16:10:54 +0000

    Init fork: change package name

--- nacl/sign/sign.go
+++ nacl/sign/sign.go
@@ -23,8 +23,8 @@ package sign
 import (
 	"io"
 
-	"golang.org/x/crypto/ed25519"
-	"golang.org/x/crypto/internal/alias"
+	"github.com/gitpod-io/golang-crypto/ed25519"
+	"github.com/gitpod-io/golang-crypto/internal/alias"
 )
 
 // Overhead is the number of bytes of overhead when signing a message.
--- ssh/test/session_test.go
+++ ssh/test/session_test.go
@@ -20,7 +20,7 @@ import (
 	"strings"
 	"testing"
 
-	"golang.org/x/crypto/ssh"
+	"github.com/gitpod-io/golang-crypto/ssh"
 )
 
 func TestRunCommandSuccess(t *testing.T) {
