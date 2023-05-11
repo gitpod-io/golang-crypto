golang/crypto

Init fork: change package name

chacha20/chacha_generic.go
@@ -12,7 +12,7 @@ import (
 	"errors"
 	"math/bits"
 
-	"golang.org/x/crypto/internal/subtle"
+	"github.com/gitpod-io/golang-crypto/internal/subtle"
 )
 
 const (

nacl/secretbox/secretbox.go
@@ -35,9 +35,9 @@ This package is interoperable with NaCl: https://nacl.cr.yp.to/secretbox.html.
 package secretbox // import "golang.org/x/crypto/nacl/secretbox"
 
 import (
-	"golang.org/x/crypto/internal/poly1305"
-	"golang.org/x/crypto/internal/subtle"
-	"golang.org/x/crypto/salsa20/salsa"
+	"github.com/gitpod-io/golang-crypto/internal/poly1305"
+	"github.com/gitpod-io/golang-crypto/internal/subtle"
+	"github.com/gitpod-io/golang-crypto/salsa20/salsa"
 )
 
 // Overhead is the number of bytes of overhead when boxing a message.

xts/xts.go
@@ -29,7 +29,7 @@ import (
 	"errors"
 	"sync"
 
-	"golang.org/x/crypto/internal/subtle"
+	"github.com/gitpod-io/golang-crypto/internal/subtle"
 )
 
 // Cipher contains an expanded key structure. It is safe for concurrent use if

gitpod-io/golang-crypto

chacha20/chacha_generic.go
@@ -12,7 +12,7 @@ import (
 	"errors"
 	"math/bits"
 
-	"golang.org/x/crypto/internal/alias"
+	"github.com/gitpod-io/golang-crypto/internal/alias"
 )
 
 const (

nacl/secretbox/secretbox.go
@@ -35,9 +35,9 @@ This package is interoperable with NaCl: https://nacl.cr.yp.to/secretbox.html.
 package secretbox // import "golang.org/x/crypto/nacl/secretbox"
 
 import (
-	"golang.org/x/crypto/internal/alias"
-	"golang.org/x/crypto/internal/poly1305"
-	"golang.org/x/crypto/salsa20/salsa"
+	"github.com/gitpod-io/golang-crypto/internal/alias"
+	"github.com/gitpod-io/golang-crypto/internal/poly1305"
+	"github.com/gitpod-io/golang-crypto/salsa20/salsa"
 )
 
 // Overhead is the number of bytes of overhead when boxing a message.

xts/xts.go
@@ -29,7 +29,7 @@ import (
 	"errors"
 	"sync"
 
-	"golang.org/x/crypto/internal/alias"
+	"github.com/gitpod-io/golang-crypto/internal/alias"
 )
 
 // Cipher contains an expanded key structure. It is safe for concurrent use if