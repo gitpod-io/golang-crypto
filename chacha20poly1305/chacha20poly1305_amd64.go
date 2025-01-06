golang/crypto

Init fork: change package name

chacha20poly1305/chacha20poly1305_amd64.go
@@ -10,7 +10,7 @@ package chacha20poly1305
 import (
 	"encoding/binary"
 
-	"golang.org/x/crypto/internal/subtle"
+	"github.com/gitpod-io/golang-crypto/internal/subtle"
 	"golang.org/x/sys/cpu"
 )
 

nacl/secretbox/secretbox.go
@@ -35,9 +35,9 @@ This package is interoperable with NaCl: https://nacl.cr.yp.to/secretbox.html.
 package secretbox // import "golang.org/x/crypto/nacl/secretbox"
 
 import (
-	"golang.org/x/crypto/internal/poly1305"
-	"golang.org/x/crypto/internal/subtle"
-	"golang.org/x/crypto/salsa20/salsa"
+	"github.com/gitpod-io/golang-crypto/internal/poly1305"
+	"github.com/gitpod-io/golang-crypto/internal/subtle"
+	"github.com/gitpod-io/golang-crypto/salsa20/salsa"
 )
 
 // Overhead is the number of bytes of overhead when boxing a message.

nacl/sign/sign.go
@@ -23,8 +23,8 @@ package sign
 import (
 	"io"
 
-	"golang.org/x/crypto/ed25519"
-	"golang.org/x/crypto/internal/subtle"
+	"github.com/gitpod-io/golang-crypto/ed25519"
+	"github.com/gitpod-io/golang-crypto/internal/subtle"
 )
 
 // Overhead is the number of bytes of overhead when signing a message.

gitpod-io/golang-crypto

chacha20poly1305/chacha20poly1305_amd64.go
@@ -9,7 +9,7 @@ package chacha20poly1305
 import (
 	"encoding/binary"
 
-	"golang.org/x/crypto/internal/alias"
+	"github.com/gitpod-io/golang-crypto/internal/alias"
 	"golang.org/x/sys/cpu"
 )
 

nacl/secretbox/secretbox.go
@@ -35,9 +35,9 @@ This package is interoperable with NaCl: https://nacl.cr.yp.to/secretbox.html.
 package secretbox
 
 import (
-	"golang.org/x/crypto/internal/alias"
-	"golang.org/x/crypto/internal/poly1305"
-	"golang.org/x/crypto/salsa20/salsa"
+	"github.com/gitpod-io/golang-crypto/internal/alias"
+	"github.com/gitpod-io/golang-crypto/internal/poly1305"
+	"github.com/gitpod-io/golang-crypto/salsa20/salsa"
 )
 
 // Overhead is the number of bytes of overhead when boxing a message.

nacl/sign/sign.go
@@ -24,7 +24,7 @@ import (
 	"crypto/ed25519"
 	"io"
 
-	"golang.org/x/crypto/internal/alias"
+	"github.com/gitpod-io/golang-crypto/internal/alias"
 )
 
 // Overhead is the number of bytes of overhead when signing a message.